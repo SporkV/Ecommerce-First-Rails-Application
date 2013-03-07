4fa4188fdb33c3c35236db1e2cdc4f3d03b136e2
Luke Porterfield <sporkv@gmail.com>
2013-03-07 13:51:16 -0600

    Added a few actions to the first controller, as well as associated views and routes

--- app/controllers/first_controller.rb
+++ app/controllers/first_controller.rb
@@ -1,2 +1,12 @@
 class FirstController < ApplicationController
+  def index
+    # for now im going to leave this empty
+  end
+  # after the index is called, the associated view will automagically be loaded.
+  # app/view/first/index/html.erb
+  
+  def about
+    # Again, for now, this action is empty
+  end
+  # After this action is called, the associated view is loaded: app/view/first/about.html.erb
 end
