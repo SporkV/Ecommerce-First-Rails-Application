99336e88f35c1b6db92f56b41a9ecd798cb3a943
Luke Porterfield <sporkv@gmail.com>
2013-03-07 12:46:10 -0600

    First commit of my example ecomm rails project. Rails app exists, as does a first controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstRailsApp::Application.config.secret_token = '1c955e3b75935d173d98675faa431a2028b018bb5ad9890701f85e4da84733ee33dea123116fb77b6ad562d60ba864dde0055d24247c4185f5b493d0eb0077ab'
